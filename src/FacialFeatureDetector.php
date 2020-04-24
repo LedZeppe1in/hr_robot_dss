587bb7d2f735ae4b5f49cfebc0cb6a34eb908851
j80 <iskander@icc.ru>
2020-04-24 18:05:27 +0800

    Minor bugbixes

--- src/FacialFeatureDetector.php
+++ src/FacialFeatureDetector.php
@@ -1970,11 +1970,11 @@ class FacialFeatureDetector
             fclose($fd);*/
         $detectedFeatures = array();
         $FaceData = $this->processingOutliers($FaceData,10,1);
-        $detectedFeatures = $this->addPoints('OUTLIER_POINTS',$FaceData,$detectedFeatures,'outlier_level_percent(10)outlier_neighbors(1)');
+        $detectedFeatures = $this->addPoints('NORM_POINTS_OUTLIER',$FaceData,$detectedFeatures,'outlier_level_percent(10)outlier_neighbors(1)');
         $FaceData = $this->processingWithMovingAverage($FaceData,3);
-        $detectedFeatures = $this->addPoints('MA_POINTS',$FaceData,$detectedFeatures,'smoth_order(3)');
+        $detectedFeatures = $this->addPoints('NORM_POINTS_OUTLIER_MA',$FaceData,$detectedFeatures,'smoth_order(3)');
         $FaceData = $this->processingWithMovingAverage($FaceData,5);
-        $detectedFeatures = $this->addPoints('MA_POINTS',$FaceData,$detectedFeatures,'smoth_order(3_5)');
+        $detectedFeatures = $this->addPoints('NORM_POINTS_OUTLIER_MA',$FaceData,$detectedFeatures,'smoth_order(3_5)');
         //        $this->saveXY2($FaceData,'m1.json');
  /*         $fd = fopen('_MA.json', "w");
               fwrite($fd,json_encode($FaceData));
